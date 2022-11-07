c7665c3601e5f01bafce00619cab7a5ebbe41365
TyomaKAV TyomaKAV <kompanijets@gmail.com>
2022-11-07 22:31:48 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -95,7 +95,8 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'blog',
         'crispy_forms', 
-        'crispy_bootstrap5'
+        'crispy_bootstrap5',
+        "debug_toolbar",
         
     ]
 
@@ -105,6 +106,7 @@ class Dev(Configuration):
     CRISPY_TEMPLATE_PACK = "bootstrap5"
 
     MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
@@ -112,7 +114,11 @@ class Dev(Configuration):
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
         # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        
     ]
+
+    INTERNAL_IPS = ["192.168.10.93"]
+
 
     ROOT_URLCONF = 'blango.urls'
 
