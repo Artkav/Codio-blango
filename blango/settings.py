8a683a0af67ca45d0fafcdbaaeedac69a9fd73b8
TyomaKAV TyomaKAV <kompanijets@gmail.com>
2022-12-11 00:03:24 +0000

    Finish pagination and django filter

--- blango/settings.py
+++ blango/settings.py
@@ -118,8 +118,7 @@ class Dev(Configuration):
         "rest_framework",
         "rest_framework.authtoken",
         "drf_yasg",
-
-        
+        "django_filters",     
         
     ]
     
@@ -137,30 +136,47 @@ class Dev(Configuration):
     # DRF Token Authentication
 
     REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-          "rest_framework.authentication.BasicAuthentication",
-          "rest_framework.authentication.SessionAuthentication",
-          "rest_framework.authentication.TokenAuthentication",
-      ],
-    "DEFAULT_PERMISSION_CLASSES": [
-          "rest_framework.permissions.IsAuthenticatedOrReadOnly"
-      ],
+        "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+        ],
+        "DEFAULT_PERMISSION_CLASSES": [
+            "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+        ],
 
-     # Add throttling
+        # Add throttling
 
-    "DEFAULT_THROTTLE_CLASSES": [
-        "blog.api.throttling.AnonSustainedThrottle",
-        "blog.api.throttling.AnonBurstThrottle",
-        "blog.api.throttling.UserSustainedThrottle",
-        "blog.api.throttling.UserBurstThrottle",
-    ],
+        "DEFAULT_THROTTLE_CLASSES": [
+            "blog.api.throttling.AnonSustainedThrottle",
+            "blog.api.throttling.AnonBurstThrottle",
+            "blog.api.throttling.UserSustainedThrottle",
+            "blog.api.throttling.UserBurstThrottle",
+        ],
 
-    "DEFAULT_THROTTLE_RATES": {
-        "anon_sustained": "500/day",
-        "anon_burst": "10/minute",
-        "user_sustained": "5000/day",
-        "user_burst": "100/minute",
-    },
+        "DEFAULT_THROTTLE_RATES": {
+            "anon_sustained": "500/day",
+            "anon_burst": "10/minute",
+            "user_sustained": "5000/day",
+            "user_burst": "100/minute",
+        },
+
+        # Add Pagination
+
+        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+        "PAGE_SIZE": 100,
+
+        # Add Django-filters
+
+        "DEFAULT_FILTER_BACKENDS": [
+            "django_filters.rest_framework.DjangoFilterBackend",
+
+            # Add ordering
+            
+            "rest_framework.filters.OrderingFilter"
+        ],
+
+        
 }
 
 
