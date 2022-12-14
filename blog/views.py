23455d257779aa18b6e3b16b7cbdb98dec90f3b8
TyomaKAV TyomaKAV <kompanijets@gmail.com>
2022-12-14 22:47:05 +0000

    Finish fetch and React hooks

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from blog.forms import CommentForm
 from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_cookie
+from django.urls import reverse
+
 
 
 # Create your views here.
@@ -13,7 +15,10 @@ from django.views.decorators.vary import vary_on_cookie
 logger = logging.getLogger(__name__)
 
 def post_table(request):
-    return render(request, "blog/post-table.html")
+    return render(
+        request, "blog/post-table.html", {"post_list_url": reverse("post-list")}
+    )
+
 
 
 
